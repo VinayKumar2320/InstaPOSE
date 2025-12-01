65a974cc804ec6a80dcb2576a3a0eb5c48bcbeb6
Vinay Kumar <64004545+VinayKumar2320@users.noreply.github.com>
2025-11-30 23:13:35 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App";
-import "./index.css";   // if you use CSS
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
